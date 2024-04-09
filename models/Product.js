49834a4c88b9ea08f4f5b4ef8d66ef59119065f5
justinravenwolfe <147559260+justinravenwolfe@users.noreply.github.com>
2024-04-09 19:58:39 -0400

    product fields are complete. via Aliyah Steele Harvard U.

--- models/Product.js
+++ models/Product.js
@@ -9,6 +9,27 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false, 
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2), 
+      allowNull: false, 
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false, 
+      defaultValue: 10, 
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+    }
     // define columns
   },
   {
@@ -17,7 +38,12 @@ Product.init(
     freezeTableName: true,
     underscored: true,
     modelName: 'product',
-  }
+  },
 );
+  Product.belongsTo(Category, {
+    foreignKey: {
+      allowNull: false // Ensures that a product must have a category
+    }
+  });
 
 module.exports = Product;
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const categoryRoutes = require('./api/category-routes');
+const productRoutes = require('./api/product-routes');
+const tagRoutes = require('./api/tag-routes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-  res.send("<h1>Wrong Route!</h1>")
-});
+// all of these routes are PREFIXED with '/api'
+router.use('/categories', categoryRoutes);
+router.use('/products', productRoutes);
+router.use('/tags', tagRoutes);
 
 module.exports = router;
+
